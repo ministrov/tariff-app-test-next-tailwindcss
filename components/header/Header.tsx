37ee5f499e56be2651a5ba790b955f5b07ccb41f
Zhila <ministrov2018@gmail.com>
2025-10-03 13:29:39 +0300

    feat: set a layout component to connect a font

---
+++ components/header/Header.tsx
@@ -0,0 +1,17 @@
+import Timer from '../timer/Timer';
+import { TimerProps } from '../timer/Timer.interface';
+
+const Header = ({ timeLeft }: TimerProps) => {
+  return (
+    <header className='fixed top-0 left-0 right-0 bg-white shadow-lg z-50'>
+      <div className='container mx-auto px-4 py-4'>
+        <div className='flex justify-between items-center'>
+          <div className='text-xl font-bold text-gray-900'>Тарифные планы</div>
+          <Timer timeLeft={timeLeft} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
